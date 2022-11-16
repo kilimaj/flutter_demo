ab4b926900ee97bdc87d25cb953b943bd6724bbe
John Kilima <jkbundala@gmail.com>
2022-11-16 14:02:17 +0300

    4 - Creating MaterialApp and Scaffolding widget part 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,26 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() {
-  runApp(Home());
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.cyan
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Demo"),
+          actions: <Widget>[
+            Icon(Icons.search),
+            Icon(Icons.shopping_cart)
+          ],
+        ),
+        drawer: Drawer(),
+        body: Home(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Icon(Icons.add),
+        ),
+      ),
+    )
+  );
 }
