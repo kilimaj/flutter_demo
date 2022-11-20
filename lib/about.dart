a6b35d5801d07c64242cddea0dd6a92311af3781
John Kilima <jkbundala@gmail.com>
2022-11-20 15:18:02 +0300

    Navigating to Screens - Part 2 - Fixing Style

--- lib/about.dart
+++ lib/about.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class About extends StatelessWidget {
   const About({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Welcome to flutter Demo"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About Screen"),
+      ),
+      body: const Center(
+        child: Text("Welcome to flutter Demo"),
+      ),
     );
   }
 }
